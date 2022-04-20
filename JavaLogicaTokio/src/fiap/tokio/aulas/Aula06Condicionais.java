f4cc73d0e1e37d48f20616d3572bd7366ffdcfe8
VeronicaMSilva <veronica.maria8793@gmail.com>
2022-04-20 18:27:16 -0300

    codigos java 02

---
+++ JavaLogicaTokio/src/fiap/tokio/aulas/Aula06Condicionais.java
@@ -0,0 +1,20 @@
+package fiap.tokio.aulas;
+
+public class Aula06Condicionais {
+
+	public static void main(String[] args) {
+		
+		float nota = 10;
+		
+		boolean condicao = nota > 6;
+		
+		System.out.println(condicao);
+		
+		if (!condicao) {
+			System.out.println(nota);
+			
+			
+		}
+	}
+
+}
